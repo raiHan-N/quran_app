8f7fd724ad300e49eb1e3585b3234a6e62baf672
ray_han <ray102303@gmail.com>
2023-01-14 14:39:51 +0700

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vuetify from "@vuetify/vite-plugin";
+import react from "@vitejs/plugin-react";
 
 const path = require("path");
 
