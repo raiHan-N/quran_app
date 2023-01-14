a774ea252dd5f2f622637dd70a4678f136651104
ray_han <ray102303@gmail.com>
2023-01-14 14:48:23 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const path = require("path");
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
-  },
+  manifest: true,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
