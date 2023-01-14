6782a05cb40a947ece2ace0ab0197e65db2deb25
ray_han <ray102303@gmail.com>
2023-01-15 00:14:34 +0700

    error vite fixing

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
-import { URL, fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    manifest: true,
-    base: "/",
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-    build: {
-      chunkSizeWarningLimit: 1600,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
