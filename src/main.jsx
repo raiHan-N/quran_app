f936e138d83bc69cfbbf716bd074f51633280317
ray_han <ray102303@gmail.com>
2023-01-15 17:17:14 +0700

    rev

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HashRouter as BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import DataProvider from "./utils/DataProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
